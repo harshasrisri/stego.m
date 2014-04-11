7c47c2e5452cf2b19c080fd2298139b452758f00
Sriharsha <harshasrisri@gmail.com>
2014-04-11 01:19:15 -0700

    renamed original file

--- oops_stego.m
+++ oops_stego.m
@@ -1,6 +1,11 @@
 stage1 = transImage (imread('test.jpg'), 'haar');
 stage2 = transImage (stage1.HH, 'haar');
-stage2 = embedMsg (stage2, 'M');
-stage2 = finalize(stage2);
-stage1.HH = stage2.aft;
+% stage2 = embedMsg (stage2, 'M');
+% stage2 = finalize(stage2);
+% stage1.HH = stage2.aft;
 stage1 = finalize(stage1, 'after.jpg');
+
+stage3 = transImage (imread('after.jpg'), 'haar');
+stage4 = transImage (stage3.HH, 'haar');
+
+[psnr, mse, me, l2r] = measerr (stage1.bef, stage3.bef);
