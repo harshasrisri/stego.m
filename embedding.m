8fc60f03d0c7c568c0c67b41582b7626dc0d6308
Sriharsha <harshasrisri@gmail.com>
2014-04-10 22:58:35 -0700

    trying oops in matlab

--- embedding.m
+++ embedding.m
@@ -1,46 +1,36 @@
 before = imread('test.jpg');
 sz1 = size(before);
+before = int16(before);
+before = before * 4;
 [LL,LH,HL,HHb] = dwt2(before, 'haar');
 
 sz2 = size(HHb);
-[HH1, HH2, HH3, HH4b] = dwt2 (uint8(HHb * 2), 'haar');
+[HH1, HH2, HH3, HH4b] = dwt2 (HHb, 'haar');
 
-HH4a = uint8(HH4b * 2);
+HH4a = int16(HH4b);
 
 msg = 'M';
 msgBinary = dec2bin(msg,8)';
 msgBinary = msgBinary(:)'-'0';
 
 for i=1:8
-%     if msgBinary(i) == 1
-%         HH4a(1,i) = bitset(HH4a(1,i),1);
-%     else if bitget(HH4a(1,i),1) == 1
-%             HH4a(1,i) = bitand(HH4a(1,i),msgBinary(i));
-%         end
-%     end
-HH4a(1,i) = msgBinary(i);
+    HH4a(1,i) = msgBinary(i);
 end
 
 HH4a = double(HH4a);
-HH4a = HH4a / 2;
-
-diff_HH4 = max(max(abs(HH4a - HH4b)));
-
 HHa = idwt2(HH1, HH2, HH3, HH4a, 'haar', sz2);
-HHa = HHa / 2;
+after = uint8(idwt2(LL, LH, HL, HHa, 'haar', sz1) / 4);
 
-diff_HH = max(max(abs(HHa - HHb)));
+[psnr_HH4, mse_HH4, maxerr_HH4, l2rat_HH4] = measerr(HH4a, HH4b);
+[psnr_HH, mse_HH, maxerr_HH, l2rat_HH] = measerr(HHa, HHb);
+[psnr_img, mse_img, maxerr_img, l2rat_img] = measerr(after, before);
 
-after = uint8(idwt2(LL, LH, HL, HHa, 'haar', sz1));
-
-diff_img = max(max(abs(before-after)));
-
-imwrite (before, 'before.jpg');
 imwrite (after, 'after.jpg');
-extract = imread ('after.jpg');
 
-[LLe, LHe, HLe, HHe] = dwt2 (extract, 'haar');
-[HH1e, HH2e, HH3e, HH4e] = dwt2 (uint8(HHe * 2), 'haar');
-HH4e = uint8(HH4e * 2);
+after = imread('after.jpg');
+after = int16(after);
+after = after * 4;
 
-diff_HHe = max(max(abs
+[LL,LH,HL,HHe] = dwt2 (after, 'haar');
+[HH1,HH2,HH3,HH4e] = dwt2 (HHe, 'haar');
+HH4e = int16(HH4e);
---
+++ transImage.m
@@ -0,0 +1,32 @@
+classdef transImage
+	properties
+		img
+		LL
+		LH
+		HL
+		HH
+		sz
+		wname
+    end
+% 
+%    methods
+% 		function ci = transImageClass (img, wname)
+% 			if nargin > 0
+%				ci.img = int16(img);
+%				ci.img = ci.img * 4;
+% 				ci.sz = size(img);
+% 				ci.wname = wname;
+% 				[ci.LL, ci.LH, ci.HL, ci.HH] = dwt2 (ci.img, ci.wname);
+% 			end
+% 		end
+% 	end
+% 
+% 	methods
+%		function inverse ()
+%			this.img = 
+% 		function show()
+% 			figure
+% 			imshow (this.img);
+% 		end
+% 	end
+ end
