6e7433bf15bed6a07b814193c24808f838da333c
Sriharsha <harshasrisri@gmail.com>
2014-04-14 21:28:49 -0700

    somewhat working. have to test further

--- embedMsg.m
+++ embedMsg.m
@@ -2,15 +2,17 @@ function obj = embedMsg (obj, message)
 msgBinary = dec2bin(message,8)';
 msgBinary = msgBinary(:)'-'0';
 
-steg_coeffs = [-0.125, 0.125];
+steg_coeffs = [10, 20, 30, 40];
 
 for ii=1:8
     if msgBinary(ii) == 1
-        if abs(obj(1,ii)) < 0.125
+        if obj(1,ii) <= 0
             obj(1,ii) = steg_coeffs(randi(numel(steg_coeffs)));
         end
     else
-        obj(1,ii) = 0.0;
+        if obj(1,ii) >= 0
+            obj(1,ii) = -1 * steg_coeffs(randi(numel(steg_coeffs)));
+        end
     end
 end
 end
