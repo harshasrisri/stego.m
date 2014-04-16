6f7864d4e9e7ca8cbe9f2e1dbff35617842b8ff4
Sriharsha <harshasrisri@gmail.com>
2014-04-16 14:55:08 -0700

    some beautification and cleanup

--- oops_stego.m
+++ oops_stego.m
@@ -1,9 +1,11 @@
-embed1 = transImage (imread('test.jpg'), 'haar');
+coverImage = imread('test.jpg');
+embedText = 'ECE573: Project Steganography By Team DesiDroid';
+
+embed1 = transImage (coverImage, 'haar');
 embed2 = transImage (embed1.HH, 'haar');
 embed3 = transImage (embed2.HH, 'haar');
 
-embed3.HH = embedMsg (embed3.HH, 'Project Steganography by Team DesiDroid');
-
+embed3.HH = embedMsg (embed3.HH, embedText);
 embed3 = finalize (embed3);
 
 embed2.HH = embed3.aft;
@@ -16,8 +18,6 @@ extract1 = transImage (imread('after.png'), 'haar');
 extract2 = transImage (extract1.HH, 'haar');
 extract3 = transImage (extract2.HH, 'haar');
 
-% [psnr, mse, me, l2r] = measerr (embed1.aft, extract1.bef);
-% [psnr, mse, me, l2r] = measerr (embed2.aft, extract2.bef);
-% [psnr, mse, me, l2r] = measerr (embed3.aft, extract3.bef);
+extractedText = extractMsg (extract3.HH);
 
-extractMsg (extract3.HH)
+sprintf ('Embedded Text:\n%s\n\nExtracted Text:\n%s', embedText, extractedText)
